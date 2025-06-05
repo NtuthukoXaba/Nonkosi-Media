e636a09b4a582370dd86b9e44f3a334d570530cd
Ntuthuko Xaba <xabantuthuko04@gmail.com>
2025-06-05 21:00:45 +0200

    Initial commit for Nonkosi Media Flask website

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/admin')
+def admin_home():
+    return render_template('admin_home.html')
+
+@app.route('/journalist')
+def journalist_home():
+    return render_template('journalist_home.html')
+
+@app.route('/login')
+def login():
+    return render_template('Login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
