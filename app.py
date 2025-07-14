21cd1811140570f2d392f5b3a2e452533f474a66
Ntuthuko Xaba <xabantuthuko04@gmail.com>
2025-07-14 21:26:09 +0200

    Database

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_migrate import Migrate
 # --- App setup ---
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///maskandi.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:yCdPFYTvIXIsLCFhzWWFAReXKAEDegxa@mainline.proxy.rlwy.net:49786/maskandidb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static/uploads/profile_pics'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
